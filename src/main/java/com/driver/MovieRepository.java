19b6d19a444f3acff6f8048c6e16324b52e3fe91
Chaitanya <chaitanya09306@gmail.com>
2022-11-03 17:13:22 +0530

    WatchList

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -8,18 +8,19 @@ import java.util.HashMap;
 import java.util.List;
 
 @Repository
+@Component
 public class MovieRepository {
     List<Movie> movies=new ArrayList<>();
     List<Director> directors=new ArrayList<>();
     HashMap<Director,List<Movie>> directorMoviePair=new HashMap<>();
 
-    void addMovieToDatabase(Movie movie){
+    public void addMovieToDatabase(Movie movie){
         movies.add(movie);
     }
-    void addDirectorToDatabase(Director director){
+    public void addDirectorToDatabase(Director director){
         directors.add(director);
     }
-    void addMovieDirectorPairToDatabase(String directorName,String movieName){
+    public void addMovieDirectorPairToDatabase(String directorName,String movieName){
         Director director=null;
         Movie movie=null;
         for(Director d:directors){
@@ -37,29 +38,29 @@ public class MovieRepository {
             directorMoviePair.put(director,list);
         }
     }
-    Movie getMovieByNameFromDatabase(String movieName){
+    public Movie getMovieByNameFromDatabase(String movieName){
         for(Movie m:movies){
             if(m.getName().equals(movieName)) return m;
         }
         return null;
     }
 
-    Director getDirectorByNameFromDatabase(String directorName){
+    public Director getDirectorByNameFromDatabase(String directorName){
         for(Director d:directors){
             if(d.getName().equals(directorName)){ return d; }
         }
         return null;
     }
-    List<Movie> getMoviesByDirectorNameFromDatabase(String directorName){
+    public List<Movie> getMoviesByDirectorNameFromDatabase(String directorName){
         for(Director d:directorMoviePair.keySet()){
             if(d.getName().equals(directorName)){ return directorMoviePair.get(d); }
         }
         return null;
     }
-    List<Movie> findAllMoviesFromDatabase(){
+    public List<Movie> findAllMoviesFromDatabase(){
         return movies;
     }
-    void deleteDirectorByNameFromDatabase(String directorName){
+    public void deleteDirectorByNameFromDatabase(String directorName){
         Director director=null;
         for(Director d:directors){
             if(d.getName().equals(directorName)){  director=d; }
@@ -71,7 +72,7 @@ public class MovieRepository {
         directorMoviePair.remove(director);
     }
 
-    void deleteAllDirectorsFromDataBase(){
+    public void deleteAllDirectorsFromDataBase(){
         movies=new ArrayList<>();
         directors=new ArrayList<>();
         directorMoviePair=new HashMap<>();
--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -10,33 +10,33 @@ public class MovieService {
 
     @Autowired
     MovieRepository movieRepository;
-    void addMovie(Movie movie){
+    public void addMovie(Movie movie){
         movieRepository.addMovieToDatabase(movie);
     }
-    void addDirector(Director director){
+    public void addDirector(Director director){
         movieRepository.addDirectorToDatabase(director);
     }
-    void addMovieDirectorPair(String directorName,String movieName){
+    public void addMovieDirectorPair(String directorName,String movieName){
         movieRepository.addMovieDirectorPairToDatabase(directorName,movieName);
     }
-    Movie getMovieByName(String movieName){
+    public Movie getMovieByName(String movieName){
         return movieRepository.getMovieByNameFromDatabase(movieName);
     }
-    Director getDirectorByName(String directorName){
+    public Director getDirectorByName(String directorName){
         return movieRepository.getDirectorByNameFromDatabase(directorName);
     }
-    List<Movie> getMoviesByDirectorName(String directorName){
+    public List<Movie> getMoviesByDirectorName(String directorName){
         return movieRepository.getMoviesByDirectorNameFromDatabase(directorName);
     }
-    List<Movie> findAllMovies(){
+    public List<Movie> findAllMovies(){
         return movieRepository.findAllMoviesFromDatabase();
     }
-    void deleteDirectorByName(String directorName){
+    public void deleteDirectorByName(String directorName){
         movieRepository.deleteDirectorByNameFromDatabase(directorName);
         return;
     }
 
-    void deleteAllDirectors(){
+    public void deleteAllDirectors(){
         movieRepository.deleteAllDirectorsFromDataBase();
 
     }
